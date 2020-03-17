1efb16081e4fbeafd3de83b635eb07c7f3ff0791
kkaushik-nnet <kaushik@stud.fra-uas.de>
2020-03-17 18:13:37 +0100

    Post Processing Script

---
+++ Localization_v_2_0/Scripts/PostProcessing/Script.sh
@@ -0,0 +1,14 @@
+#  Indoor recordings - Change paths 
+
+#ln -s  indoorMap.csv /hri/localdisk/postProcessNew//indoorMap.csv
+python processMiimoRecordings.py /hri/localdisk/ThesisProject/Kaushik/Kaushik/Experiment_10_2/Training_Images/
+
+
+#  Design garden recordings - Change paths
+#ln -s /home/mhzafar/Downloads/Software/MapData/Designgarden/160316_16703_bigGarden_lokal.PKT /hri/localdisk/Haris/20170627/sub5//map.PKT
+#python processMiimoRecordings.py /hri/localdisk/Haris/20170627/sub5
+
+
+#  Small garden recordings - Change paths/
+# ln -s /home/mhzafar/Downloads/Software/MapData/O4_garden/160316_16703_kleinerGarten_lokal.PKT /hri/localdisk/Haris/20170721/sub15/map.PKT
+# python processMiimoRecordings.py /hri/localdisk/Haris/20170721/sub15/
